c86477ee9fe940bcc34ccc3f7a7ab7bdfce75bf2
Thive-N <35178093+Thive-N@users.noreply.github.com>
2026-04-05 16:20:58 +0100

    Initial Tauri commit

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,8 @@
 fn main() {
-  tauri_build::build()
+    // https://github.com/tauri-apps/tauri/issues/10702
+    #[cfg(target_os = "linux")]
+    unsafe {
+        std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+        std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+    }
 }
